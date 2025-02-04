fcc332bc325933e756f4d3592e767150a7e4fb4b
Srivalli-1 <kajasrivalli5@gmail.com>
2025-02-04 11:13:45 +0530

    done

--- BACKEND/model/user.js
+++ BACKEND/model/user.js
@@ -1,25 +1,36 @@
-const mongoose =require('mongoose')
+const mangoose = require('mangoose')
+const bcrypt = require('bcrypt js')
+const jwt=require('jsonwebtoken')
 
-const userSchema = new mongoose.Schema({
-    name:{type:String,Required:true},
+const userSchema = new mangoose.Schema({
+    name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
-    phonenumber:{type:Number},
-    password:{type:String,required:true,minlenght:4},
+    phoneNumber:{type:Number},
+    password:{type:String,required:true,minLenght:4},
     avatar:{
         id:{type:String},
         url:{type:String}
     },
-    address:{
+    address:[
         {
             country:{type:String,required:true},
-            city:{type;String , required:true},
+            city:{type:String,required:true},
             address1:{type:String},
             address2:{type:String},
-            pincode:{type:Number,required:true}
+            pincode1:{type:Number,required:true}
         }
-    },
-    role:{type:String,defaul
+    ],
+    role:{type:String,default:user},
+    createAT:{type:DataTransfer, default:DataTransfer.now()}
 
-
-    }
 })
+
+userSchema.pre('save',async function(next) {
+if(!this.modified("password"))
+    return next()
+  await bcrypt.hash(this.password,10)
+  next()  
+})
+userSchema.methods.jsonTokens=function(){
+    return jwt.sign({id:this._id},process.env.JWT_TOKEN,{expireIn1:process.env.Jwt_EXPIRES})
+}
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,15 +1,17 @@
 const app = require('./app')
 const connectDB=require('./db/db')
+
 process.on('uncaughtException',(error)=>{
-    console.log(`server shutthing down ${error.message}`)
-    process.exit(1)
+    console.log(server shutthing down ${error.message})
+    process.exit(0)
 })
- if(process.env.NODE_ENV !== 'PRODUCTION'){
-     require('dotenv').config({path:'config/.env'})}
- 
+
+if(process.env.NODE_ENV !== 'PRODUCTION')
+    require('dotenv').config({path:'config/.env'})
+    
 connectDB()
-app.listen(process.env.PORT,()=>{
-    console.log(`Server suceessfully listen at port http://localhost:${process.env.PORT}`)
+
+
+const server=app.listen(process.env.PORT,()=>{
+    console.log(Server suceessfully listen at port http://localhost:${process.env.PORT})
 })
-            
-process.on('unhandledRejection',()=>{process.exit(1)})
